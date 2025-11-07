8ebcc3ef2d35b430af1fd685da5c0d21fb8b47b3
rashmi <rashmicjannu@gmail.com>
2025-11-07 17:50:19 +0530

    05: ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 "use client";
 import { toast } from "sonner";
+import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import {Button} from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
+import {Input} from "@/components/ui/input";
+
 
 
 const Page = () => {
+   const [value, setValue] = useState("");
+
    const trpc = useTRPC();
     const invoke = useMutation(trpc.invoke.mutationOptions({
       onSuccess: () => {
-        toast.success("bg started");
+        toast.success("background job started");
       }
     }));
 
   return (
     
     <div className="p-4 max-w-9xl mx-auto">
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({text: "frontt"})}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({value: value})}>
         Invoke bg job
         </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,22 @@
+import {  anthropic, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    // return { message: `Hello ${event.data.email}!` };
-    //2
-     await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({event}) => {
+      const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer.  You write readable, maintainable code. You write simple Next.js and React snippets.",
+       model: anthropic({ 
+        model: "claude-3-5-haiku-latest",
+        defaultParameters: {max_tokens: 4096 } // Add the required property
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+    return {output};
   },
 );
